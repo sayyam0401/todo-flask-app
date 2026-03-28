0e2a4ad7beaa689af646438b161f9ba321d8f492
Sayyam Lohade <sayyamlohade@gmail.com>
2026-03-28 21:57:04 +0530

    Initial commit - ToDo Flask App

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+def get_db():
+    return sqlite3.connect("database.db")
+
+@app.route("/")
+def home():
+    db = get_db()
+    tasks = db.execute("SELECT * FROM tasks").fetchall()
+    db.close()
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add():
+    task = request.form["task"]
+    db = get_db()
+    db.execute("INSERT INTO tasks (name) VALUES (?)", (task,))
+    db.commit()
+    db.close()
+    return redirect("/")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    db = get_db()
+    db.execute("DELETE FROM tasks WHERE id=?", (id,))
+    db.commit()
+    db.close()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
+
